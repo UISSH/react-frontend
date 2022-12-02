dde2022a8d6cbab2851b8c6d713a3afd1d15f318
zmaplex <zmaplex@gmail.com>
2022-12-02 21:53:18 +0800

    Added: terminal footer.

--- src/components/terminal/HostsIndex.tsx
+++ src/components/terminal/HostsIndex.tsx
@@ -1,14 +1,15 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { json } from "react-router-dom";
 import { KVStorage } from "../../requests/utils";
 import Button from "@mui/material/Button";
 import { Divider } from "@mui/material";
 
 export interface HostsIndexProps {
+  sshClientInfo: SSHClientInfo;
   children?: React.ReactNode;
 }
 
-interface SSHClientInfo {
+export interface SSHClientInfo {
   [name: string]: SSHClient;
 }
 
@@ -24,29 +25,18 @@ interface SSHClient {
 }
 
 export default function HostsIndex(props: HostsIndexProps) {
-  const [sshClient, setSshClient] = useState<SSHClientInfo>({});
-  useEffect(() => {
-    let kv = new KVStorage("SSH_CLIENT");
-    kv.init().then((val) => {
-      console.log(val);
-      if (val == null) {
-        setSshClient({});
-      } else {
-        setSshClient(JSON.parse(val));
-      }
-    });
-  }, []);
   return (
     <>
       <Divider>Host</Divider>
       <div className="flex gap-1">
-        {Object.keys(sshClient).map((item) => {
-          return (
-            <Button variant="contained" color="secondary">
-              {item}
-            </Button>
-          );
-        })}
+        {props.sshClientInfo &&
+          Object.keys(props.sshClientInfo).map((item) => {
+            return (
+              <Button variant="contained" color="secondary">
+                {item}
+              </Button>
+            );
+          })}
       </div>
     </>
   );
--- src/pages/indexLayout/terminal.tsx
+++ src/pages/indexLayout/terminal.tsx
@@ -1,21 +1,90 @@
-import { useLoaderData } from "react-router-dom";
-import SystemStatus from "../../components/overview/SystemStatus";
-import HostsIndex from "../../components/terminal/HostsIndex";
+import HostsIndex, {
+  SSHClientInfo,
+} from "../../components/terminal/HostsIndex";
 
-export async function loader(props: { params: object }) {
-  let data = useLoaderData();
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+import Tabs from "@mui/material/Tabs";
+import Typography from "@mui/material/Typography";
+import * as React from "react";
+import { KVStorage } from "../../requests/utils";
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
 }
 
-export async function action(props: { params: any; request: any }) {
-  let formData = await props.request.formData();
-}
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
 
-export default function Index() {
   return (
-    <>
-      <div>
-        <HostsIndex></HostsIndex>
-      </div>
-    </>
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}>
+      {value === index && <div>{children}</div>}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
+export default function BasicTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+  const [sshClient, setSshClient] = React.useState<SSHClientInfo>({});
+  const initFlag = React.useRef(true);
+  React.useEffect(() => {
+    if (!initFlag.current) {
+      return;
+    }
+
+    let kv = new KVStorage("SSH_CLIENT");
+    kv.init().then((val) => {
+      if (val == null) {
+        setSshClient({});
+      } else {
+        setSshClient(JSON.parse(val));
+      }
+    });
+    return () => {
+      initFlag.current = false;
+    };
+  }, []);
+  return (
+    <div>
+      <Box sx={{ width: "100%" }}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            aria-label="terminal tabs ">
+            <Tab label="Host" {...a11yProps(0)} />
+            <Tab label="Item Two" {...a11yProps(1)} />
+            <Tab label="Item Three" {...a11yProps(2)} />
+          </Tabs>
+        </Box>
+        <TabPanel value={value} index={0}>
+          <HostsIndex sshClientInfo={sshClient}></HostsIndex>
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          Item Two
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          Item Three
+        </TabPanel>
+      </Box>
+    </div>
   );
 }
