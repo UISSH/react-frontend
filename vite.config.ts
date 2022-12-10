97299e08335c620a265dd1dd7bc1cc01abceb230
zmaplex <zmaplex@gmail.com>
2022-12-10 13:51:09 +0800

    Updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "localhost",
+    port: 8080,
+  },
   plugins: [react()],
-})
+});
